658f5af0b3249fe5927013d36036aa19155be368
vegirevathi <vegirevathi@gmail.com>
2020-06-17 15:23:38 +0530

    ADDED: Welcome Message to the User

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Advanced Address Book");// write ySysour code here
+    }
+}
